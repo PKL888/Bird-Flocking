870213046021fb6131261c0d3163620e5b40de1f
PKL888 <pelegkl888@gmail.com>
2018-06-08 17:31:33 +1000

    Updated program

--- birds.py
+++ birds.py
@@ -11,11 +11,15 @@ import sys
 pygame.init()
 
 size = width, height = 800, 600
-white = 255, 255, 255
 black = 0, 0, 0
+white = 255, 255, 255
+blue = 0, 0, 255
+light_blue = 0, 0, 200
+purple = 255, 0, 255
+greyish = 50, 50, 50
 
 maxVelocity = 10
-numBirds = 50
+numBirds = 1
 birds = []
 
 
@@ -131,8 +135,39 @@ class Bird:
 screen = pygame.display.set_mode(size)
 
 mybird = pygame.image.load("bluebird2.png")
-mybird = pygame.transform.scale(mybird, (5, 5))
+mybird = pygame.transform.scale(mybird, (7, 7))
 mybirdrect = mybird.get_rect()
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def button(msg, x, y, w, h, inactive_colour, active_colour, numBirds):
+    # mouse interaction |
+    mouse = pygame.mouse.get_pos()
+    print mouse
+
+    click = pygame.mouse.get_pressed()
+    print click
+
+    # if x + width > mouse[x] > x and y + height > mouse[y] > y:
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(screen, inactive_colour, (x, y, w, h))
+
+        if click[0] == 1 and numBirds is not None:
+            birds.append(Bird(random.randint(0, width), random.randint(0, height)))
+            # numBirds += 1
+            print "Yay!"
+    else:
+        pygame.draw.rect(screen, active_colour, (x, y, w, h))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    textSurface, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    screen.blit(textSurface, textRect)
+
 
 # create the birds in random positions
 for i in range(numBirds):
@@ -187,5 +222,31 @@ while 1:
 
         screen.blit(mybird, mybirdrect)
 
+    # The adding more birds button -->
+    button("Birds", 25, 25, 88, 88, blue, light_blue, numBirds)
+
+    # Counting how many birds there are -->
+    myBirdsNum = 0
+
+    for i in birds:
+        myBirdsNum += 1
+
+    print myBirdsNum
+
+    # The current amount of birds bubble -->
+    pygame.draw.rect(screen, greyish, (500, 25, 250, 50))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 10)
+    ttextSurface, ttextRect = text_objects("Current amount of birds:", smallText)
+    ttextRect.center = ((500 + (250 / 2)), (25 + (50 / 2)))
+    screen.blit(ttextSurface, ttextRect)
+
+    # The actual quantity -->
+    smallText = pygame.font.Font("freesansbold.ttf", 11)
+    stextSurface, stextRect = text_objects("\n".format(myBirdsNum), smallText)
+    stextRect.center = ((700 + (50 / 2)), (25 + (50 / 2)))
+    screen.blit(stextSurface, stextRect)
+
+    # Displaying...
     pygame.display.flip()
     pygame.time.delay(10)
